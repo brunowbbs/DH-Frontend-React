00450356b434894a287187b1936779a1eb115a51
Wesley Bruno <engwesleybruno@gmail.com>
2022-10-31 20:44:19 -0300

    aula 7

---
+++ aula-07/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const App = () => {
+  const [items, setItems] = useState([5]);
+
+  function addItem() {
+    setItems([...items, items.lastIndexOf()]);
+  }
+
+  return (
+    <>
+      <ul>
+        {items.map((item) => (
+          <li>{`O item ${item} foi adicionado à lista!`}</li>
+        ))}
+      </ul>
+      <button onClick={addItem}>Aperte</button>
+    </>
+  );
+};
+
+export default App;
